ed567ca4e6600576703403d238eca3202477ae7c
Fabiano Bhering <fabianobhering@gmail.com>
2023-11-29 11:00:57 -0300

    Create websocket.js

---
+++ projeto/websocket.js
@@ -0,0 +1,39 @@
+const mqtt = require('mqtt');
+const WebSocket = require('ws');
+
+// Configuração do servidor MQTT
+const mqttBrokerUrl = 'mqtt://192.168.64.11';
+const mqttTopic = 'sensor/dados'; // Substitua pelo tópico MQTT real
+
+const mqttClient = mqtt.connect(mqttBrokerUrl);
+
+// Configuração do servidor WebSocket
+const wss = new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', (ws) => {
+    console.log('Cliente WebSocket conectado.');
+
+    // Assina o tópico MQTT quando um cliente se conecta
+    mqttClient.subscribe(mqttTopic);
+
+    // Envia dados MQTT para o cliente WebSocket
+    mqttClient.on('message', (topic, message) => {
+        console.log('Recebeu o dado: '+message.toString());
+        const sensorData = JSON.parse(message.toString());
+        sendWebSocketData(ws, sensorData);
+    });
+
+    // Fecha a conexão quando o cliente WebSocket se desconecta
+    ws.on('close', () => {
+        console.log('Cliente WebSocket desconectado.');
+        mqttClient.unsubscribe(mqttTopic);
+    });
+});
+
+function sendWebSocketData(ws, data) {
+    // Envia os dados para o cliente WebSocket
+    console.log('Enviou dado: '+JSON.stringify(data));
+    ws.send(JSON.stringify(data));
+}
+
+console.log('WebSocket iniciado na porta 3000.');
