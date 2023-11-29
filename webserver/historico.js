7c5208058825b5d02ddb726e6de0d4cfcba8dc63
Fabiano Bhering <fabianobhering@gmail.com>
2023-11-29 10:54:28 -0300

    Update historico.js

--- webserver/historico.js
+++ webserver/historico.js
@@ -1,27 +1,61 @@
 const express = require('express');
+const fs = require('fs/promises');
+const mqtt = require('mqtt');
 const app = express();
 var cors = require('cors');
 app.use(express.static("public"));
 app.use(cors());
 
-// Dados simulados para o histórico
-// Adicione o código para obter os dados do banco de dados
-const sensorHistory = [
-    { sensor: 'sensor1', value: 23, timestamp: '2023-01-01T12:00:00Z' },
-    { sensor: 'sensor1', value: 25, timestamp: '2023-01-01T12:05:00Z' },
-    { sensor: 'sensor1', value: 28, timestamp: '2023-01-01T12:10:00Z' },
-    
-];
+// Configuração do MQTT
+const mqttBrokerUrl = 'mqtt://192.168.64.11';
+const mqttTopic = 'sensor/dados'; // Substitua pelo tópico MQTT real
+const mqttClient = mqtt.connect(mqttBrokerUrl);
 
-// Adicione um subscrição MQTT para armazenar os dados no banco de dados
+mqttClient.on('connect', () => {
+    console.log('Conectado ao broker MQTT');
+    mqttClient.subscribe(mqttTopic);
+});
+
+mqttClient.on('message', (topic, message) => {
+     console.log('Recebeu: '+message.toString());
+     const sensorData = JSON.parse(message.toString());
+     storeSensorData(sensorData);
+
+});
 
 // Rota para obter o histórico
-app.get('/api/historico', (req, res) => {
-    res.json(sensorHistory);
+app.get('/api/historico', async (req, res) => {
+    try {
+        // Lê os dados do arquivo
+        const data = await fs.readFile('historico.json', 'utf-8');
+        const sensorHistory = JSON.parse(data);
+        res.json(sensorHistory);
+    } catch (error) {
+        console.error('Erro ao ler o histórico:', error);
+        res.status(500).json({ error: 'Erro ao obter o histórico' });
+    }
 });
+
+// Função para armazenar dados do sensor no arquivo JSON
+function storeSensorData(data) {
+    fs.readFile('historico.json', 'utf-8')
+        .then((currentData) => {
+            const sensorHistory = currentData ? JSON.parse(currentData) : [];
+
+            //const sensorHistory = JSON.parse(currentData);
+            sensorHistory.push(data);
+            return fs.writeFile('historico.json', JSON.stringify(sensorHistory, null, 2), 'utf-8');
+        })
+        .then(() => {
+            console.log('Dado do sensor armazenado com sucesso');
+        })
+        .catch((error) => {
+            console.error('Erro ao armazenar o dado do sensor:', error);
+        });
+}
 
 const port = 3000; // Escolha a porta desejada
 
 app.listen(port, () => {
-    console.log(`Servidor Express iniciado na porta ${port}`);
+    console.log(`WebServer iniciado na porta ${port}`);
 });
