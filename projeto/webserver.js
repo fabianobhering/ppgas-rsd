6250df99c59a907a97f58f7c2ebd53bbae29b598
Fabiano Bhering <fabianobhering@gmail.com>
2024-10-31 10:05:24 -0300

    Update webserver.js

--- projeto/webserver.js
+++ projeto/webserver.js
@@ -3,6 +3,7 @@ const fs = require('fs/promises');
 const mqtt = require('mqtt');
 const app = express();
 var cors = require('cors');
+const mysql = require('mysql2');
 app.use(express.static("public"));
 app.use(cors());
 
@@ -23,33 +24,38 @@ mqttClient.on('message', (topic, message) => {
 
 });
 
-// Rota para obter o histórico
-app.get('/api/historico', async (req, res) => {
-    try {
-        // Lê os dados do arquivo
-        const data = await fs.readFile('historico.json', 'utf-8');
-        const sensorHistory = JSON.parse(data);
-        res.json(sensorHistory);
-    } catch (error) {
-        console.error('Erro ao ler o histórico:', error);
-        res.status(500).json({ error: 'Erro ao obter o histórico' });
-    }
-});
+//Configuração do MySQL
+const db ={
+    host     : 'ip-mysql',
+    port     : 3306,
+    user     : '00000000000',
+    password : '00000000000',
+    database : '00000000000'
+  }
+
+function execSQLQuery(sqlQry, id, res){
+  const connection = mysql.createConnection(db);
+  
+  connection.query(sqlQry, id, (error, results, fields) => {
+    
+      if(error) 
+        res.json(error);
+      else
+        res.json(results);
+    
+      connection.end();
+      console.log('executou!');
+  });
+}
+
+app.get('/api/historico', (req, res) => {
+    execSQLQuery('SELECT * FROM sensor', null, res);
+})
 
 // Função para armazenar dados do sensor no arquivo JSON
 function storeSensorData(data) {
-    fs.readFile('historico.json', 'utf-8')
-        .then((currentData) => {
-            const sensorHistory = currentData ? JSON.parse(currentData) : [];
-            sensorHistory.push(data);
-            return fs.writeFile('historico.json', JSON.stringify(sensorHistory, null, 2), 'utf-8');
-        })
-        .then(() => {
-            console.log('Dado do sensor armazenado com sucesso');
-        })
-        .catch((error) => {
-            console.error('Erro ao armazenar o dado do sensor:', error);
-        });
+    const id =[data];
+    execSQLQuery('INSERT INTO sensor(date, value) VALUES(now(), ?)',id, res);
 }
 
 const port = 3000; // Escolha a porta desejada
